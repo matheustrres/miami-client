557b6bfa116fe6005d057c90b858aac858469cae
Matheus <torresmiranda.rj@gmail.com>
2023-07-21 16:54:08 -0300

    chore: add MAIN_GUILD_ID env key

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
 type EnvConfigProps = {
 	discordToken: string;
+	mainGuildId: string;
 };
 
 export const envConfig: EnvConfigProps = {
 	discordToken: process.env.DISCORD_TOKEN as string,
+	mainGuildId: process.env.MAIN_GUILD_ID as string,
 };
--- src/typings/environment.d.ts
+++ src/typings/environment.d.ts
@@ -4,6 +4,7 @@ declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			DISCORD_TOKEN: string;
+			MAIN_GUILD_ID: string;
 		}
 	}
 }
