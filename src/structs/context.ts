a975c5b84ccdd9a0ac075faeb5a8166a1fff0a16
Matheus <torresmiranda.rj@gmail.com>
2023-07-21 15:54:16 -0300

    feat: create command Context

---
+++ src/structs/context.ts
@@ -0,0 +1,46 @@
+import {
+	type APIInteractionGuildMember,
+	ChatInputCommandInteraction,
+	type Guild,
+	type GuildMember,
+	type InteractionReplyOptions,
+	type InteractionResponse,
+	type Message,
+	type TextBasedChannel,
+	type User,
+} from 'discord.js';
+
+import MiamiClient from './client';
+
+export default class Context {
+	constructor(
+		private readonly client: MiamiClient,
+		private readonly interaction: ChatInputCommandInteraction,
+	) {}
+
+	get channel(): TextBasedChannel | null {
+		return this.interaction.channel;
+	}
+
+	get guild(): Guild | null {
+		return this.interaction.guild;
+	}
+
+	get member(): GuildMember | APIInteractionGuildMember | null {
+		return this.interaction.member;
+	}
+
+	get user(): User {
+		return this.interaction.user;
+	}
+
+	public async reply(
+		options: InteractionReplyOptions,
+	): Promise<Message | InteractionResponse> {
+		if (!options.content) options.content = '';
+
+		return this.interaction.deferred
+			? await this.interaction.editReply(options)
+			: await this.interaction.reply(options);
+	}
+}
