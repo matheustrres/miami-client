6e83da62158c53704383c304d14a716a13bff634
Matheus <torresmiranda.rj@gmail.com>
2023-07-28 21:07:53 -0300

    fix: env variable names

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ type EnvConfigProps = {
 
 export const envConfig: EnvConfigProps = {
 	discordClientToken: process.env.DISCORD_CLIENT_TOKEN as string,
-	discordMainGuildId: process.env.MAIN_GUILD_ID as string,
-	discordOwnerId: process.env.OWNER_ID as string,
+	discordMainGuildId: process.env.DISCORD_MAIN_GUILD_ID as string,
+	discordOwnerId: process.env.DISCORD_OWNER_ID as string,
 	messariApiKey: process.env.MESSARI_API_KEY as string,
 };
